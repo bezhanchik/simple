2d4577a4c22ed6e8540b1d6b000719eda10ff3ce
Newman <nepylox@gmail.com>
2026-03-06 23:52:24 +0300

    Настроил маршрутизацию и минимальные стили

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { GeneralComponent } from './general-component/general-component';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'general', component: GeneralComponent}
 ];
