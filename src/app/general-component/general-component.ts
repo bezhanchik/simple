50f042903158fdabe73ebbf07f85487ee43b476a
Newman <nepylox@gmail.com>
2026-03-07 00:32:56 +0300

    Задал настройки отображения чекбоксов

--- src/app/general-component/general-component.ts
+++ src/app/general-component/general-component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Item {
+  id: number,
+  value: number,
+  isChecked: boolean,
+  title: string
+}
 
 @Component({
   selector: 'app-general-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './general-component.html',
   styleUrl: './general-component.css',
 })
 export class GeneralComponent {
-
+    items: Item[] = [
+    {id: 1, value: 50, isChecked: false, title: 'Item1'},
+    {id: 2, value: 25, isChecked: false, title: 'Item2'},
+    {id: 3, value: 30, isChecked: false, title: 'Item3'},
+    {id: 4, value: 45, isChecked: false, title: 'Item4'}
+  ]
 }
+
